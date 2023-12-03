8c86fd2d592b631b7c2cb51792cbb17493c97499
Inferno-idk-what-to-do <matthewfiedler19@gmail.com>
2023-12-03 18:17:27 -0500

    finished part2

--- src/day3.rs
+++ src/day3.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use aoc_tools;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Number {
     x: usize,
     y: usize,
@@ -86,9 +86,17 @@ impl Number {
 
         return num_str.parse::<i32>().unwrap();
     }
+
+    fn contains(&self, row: i32, col: i32) -> bool {
+        if self.x as i32 == row {
+            return self.y as i32 <= col && ((self.y+self.length) as i32) > col; 
+        }
+
+        return false;
+    }
 }
 
-fn find_numbers(schematic: Vec<String>) -> Vec<Number> {
+fn find_numbers(schematic: &Vec<String>) -> Vec<Number> {
     let mut numbers: Vec<Number> = Vec::new();
 
     for row in 0..schematic.len() {
@@ -120,15 +128,68 @@ fn find_numbers(schematic: Vec<String>) -> Vec<Number> {
     numbers
 }
 
+#[derive(Debug)]
+struct Gear {
+    x: usize,
+    y: usize,
+}
+
+impl Gear {
+    fn ratio(&self, nums: &Vec<Number>, schematic: &Vec<Vec<char>>) -> Option<i32> {
+        let mut near: Vec<Number> = Vec::new();
+
+        for num in nums {
+            if num.contains(self.x as i32 -1, self.y as i32 -1) { near.push(*num); }
+            else if num.contains(self.x as i32 -1, self.y as i32) { near.push(*num); }
+            else if num.contains(self.x as i32 -1, self.y as i32 +1) { near.push(*num); }
+            else if num.contains(self.x as i32 , self.y as i32 -1) { near.push(*num); }
+            else if num.contains(self.x as i32 , self.y as i32 +1) { near.push(*num); }
+            else if num.contains(self.x as i32 +1, self.y as i32 -1) { near.push(*num); }
+            else if num.contains(self.x as i32 +1, self.y as i32) { near.push(*num); }
+            else if num.contains(self.x as i32 +1, self.y as i32 +1) { near.push(*num); }
+        }
+
+        if near.len() == 2 {
+            return Some(near[0].value(schematic) * near[1].value(schematic));
+        } else {
+            return None; 
+        }
+    }
+}
+
+fn gears(schematic: &Vec<Vec<char>>) -> Vec<Gear> {
+    let mut gears = Vec::new();
+
+    for row in 0..schematic.len() {
+        for col in 0..schematic[0].len() {
+            let c = schematic[row][col];
+
+            if c == '*' { gears.push(Gear { x: row, y: col}); }
+        }
+    }
+
+    return gears;
+}
+
 fn main() {
     let data: Vec<String> = aoc_tools::read_lines("data/day3.txt").unwrap();
     let data_chars = data.iter().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
 
-    let part1 = find_numbers(data).iter()
+    let part1 = find_numbers(&data).iter()
                                   .filter(|number| number.verify(&data_chars))
                                   .map(|num_filtered| num_filtered.value(&data_chars))
                                   .collect::<Vec<i32>>()
                                   .iter()
                                   .sum::<i32>();
     println!("{}", part1);
+
+    let numbers = find_numbers(&data).iter()
+                                    .filter(|number| number.verify(&data_chars))
+                                    .map(|num_borrowed| num_borrowed.clone())
+                                    .collect::<Vec<Number>>();
+
+    let gears = gears(&data_chars);
+
+    let part2 = gears.iter().map(|gear| gear.ratio(&numbers, &data_chars).unwrap_or(0)).sum::<i32>();
+    println!("{}", part2);
 }
